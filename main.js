cb4c6f817f0f0dd64ca1faa0acff263dad8da448
L1224rd <edermarques96@gmail.com>
2018-01-01 14:24:53 -0200

    added energy loss and horizontal moviment

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 var ball = document.getElementById('ball').style;
+let vSpeed = 0.4;
+let hSpeed = 1;
+let topBall = 50;
+let leftBall = 100;
 
-function ballDown(y = 50) {
+function ballDown(y = topBall, x = leftBall) {
+    ball.left = x + 'px';
     ball.top = y + '%';
     setTimeout(() => {
         if (y < 82.1) {
-            y += 0.1;
-            ballDown(y);
+            y += vSpeed;
+            x += hSpeed;
+            ballDown(y, x);
         } else {
-            ballUp();
+            topBall += 5;
+            if (topBall <= 82.1) {
+                setTimeout(() => {
+                    ballUp(y, x);
+                }, 50);
+            }
         }
     }, 1);
 }
 
-function ballUp(y = 82.1) {
+function ballUp(y = 82.1, x = leftBall) {
+    ball.left = x + 'px';
     ball.top = y + '%';
     setTimeout(() => {
-        if (y > 50) {
-            y -= 0.1;
-            ballUp(y);
+        if (y > topBall) {
+            y -= vSpeed;
+            x += hSpeed;
+            ballUp(y, x);
         } else {
-            ballDown();
+            setTimeout(() => {
+                ballDown(y, x);
+            }, 10);
         }
     }, 1);
 }
