7c8a62a07c57aff108ab717f365932297b5b0aae
L1224rd <edermarques96@gmail.com>
2018-01-02 09:56:09 -0200

    changed hSpeed deacceleration

--- main.js
+++ main.js
@@ -1,38 +1,40 @@
 var ball = document.getElementById('ball').style;
 let g = 0.5;
-let hSpeed = 1.7;
+let hSpeed = 1.76;
 let vSpeed = 7;
 let hAcceleration = -0.0013;
 
 
-function fall(x = 30, y = 10) {
-    if(hSpeed > 0) hSpeed += hAcceleration;
-    else hSpeed = 0;
-    ball.left = x + 'px';
-    g += 0.01;
-    setTimeout(() => {
-        ball.top = y + '%';
-        if (y < 81) {
-            fall(x + hSpeed, y + g);
-        } else {
-            jump(x + hSpeed);
-        }
-    }, vSpeed);
+function fall(x = 30, y = 0) {
+    if (hSpeed > 0) {
+        hSpeed += hAcceleration;
+        ball.left = x + 'px';
+        g += 0.01;
+        setTimeout(() => {
+            ball.top = y + '%';
+            if (y < 81) {
+                fall(x + hSpeed, y + g);
+            } else {
+                jump(x + hSpeed);
+            }
+        }, vSpeed);
+    }
 }
 
-function jump(x, y = 82) {
-    if(hSpeed > 0) hSpeed += hAcceleration;
-    ball.left = x + 'px';
-    g -= 0.014;
-    let last = y;
-    setTimeout(() => {
-        ball.top = y + '%';
-        if (g >= 0) {
-            jump(x + hSpeed, y - g);
-        } else {
-            fall(x + hSpeed, y);
-        }
-    }, vSpeed);
+function jump(x, y = 82.2) {
+    if (hSpeed > 0) {
+        hSpeed += hAcceleration;
+        ball.left = x + 'px';
+        g -= 0.014;
+        setTimeout(() => {
+            ball.top = y + '%';
+            if (g >= 0) {
+                jump(x + hSpeed, y - g);
+            } else {
+                fall(x + hSpeed, y);
+            }
+        }, vSpeed);
+    }
 }
 
 fall();
