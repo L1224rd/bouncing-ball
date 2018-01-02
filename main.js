3c9c780d1f2e97b4de2435b6e73b5da5f2bad2f6
L1224rd <edermarques96@gmail.com>
2018-01-02 12:29:45 -0200

    Added ball img and dynamic rotation

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 var ball = document.getElementById('ball').style;
 let g = 0.5;
-let hSpeed = 1.76;
-let vSpeed = 7;
+let hSpeed = 1.84;
+let vSpeed = 6;
 let hAcceleration = -0.00121;
-let ground = 82.1;
+let ground = 83.2;
 
 
 function fall(x = 30, y = 0) {
-    console.log(1);
+    ball.transform = 'rotate('+x+'deg)';
+    console.log(hSpeed, y);
     if (hSpeed > 0) hSpeed += hAcceleration;
-    else hSpeed = 0;
+    else if (y < ground) {
+        hSpeed = 0;
+        y = ground;
+        return;
+    }
     ball.left = x + 'px';
     g += 0.01;
     setTimeout(() => {
@@ -24,9 +29,13 @@ function fall(x = 30, y = 0) {
 
 
 function jump(x, y = ground) {
-    console.log(0);
+    ball.transform = 'rotate('+x+'deg)';
     if (hSpeed > 0) hSpeed += hAcceleration;
-    else hSpeed = 0;
+    else if (y < ground) {
+        hSpeed = 0;
+        y = ground;
+        return;
+    }
     ball.left = x + 'px';
     g -= 0.014;
     setTimeout(() => {
@@ -34,7 +43,6 @@ function jump(x, y = ground) {
         if (g >= 0) {
             jump(x + hSpeed, y - g);
         } else {
-            console.log(y);
             fall(x + hSpeed, y);
         }
     }, vSpeed);
