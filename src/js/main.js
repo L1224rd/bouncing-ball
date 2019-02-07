d1b176da6fcaf9a94ee646762dc9a5f5f8f123d6
Eder Marques <eder.marques@citi.org.br>
2019-02-07 09:17:50 -0300

    remove setSize function

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,10 @@ let mouse = require('./mouse.js'); // mouse events module
 let moviment = require('./moviment.js'); // moviment module
 
 function init(scene = false) { // adds eventlisteners to DOM elements
-    window.addEventListener('resize', scenario.setBallSize); // sets the ball size dynamicly
     window.addEventListener('mouseup', moviment.dropBall); // drop the ball
     window.addEventListener('mousemove', mouse.setMouseCoords); // drag the ball and follow the mouse position
     document.getElementById('ball').addEventListener('mousedown', moviment.grabBall); // grab ball
     if (scene) scenario.backScene(); // puts an background image on the page
-    scenario.setBallSize(); // resize ball on init
 }
 
 init();
--- src/js/scenario.js
+++ src/js/scenario.js
@@ -5,9 +5,4 @@ scenario.backScene = function() { // adds an image as background
     document.body.style.backgroundImage = "url('https://images.freeimages.com/images/large-previews/132/yellow-road-1354805.jpg')";
     document.body.style.backgroundPositionY = "-290px";
     document.getElementById('ground').style.visibility = "hidden";
-}
-
-scenario.setBallSize = function() { // sets ball's width and height to 5% of the body width
-    ball.width = '5%';
-    ball.height = (5 * document.body.clientWidth) / 100;
-}
+};
